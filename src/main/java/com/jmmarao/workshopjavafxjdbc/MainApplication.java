cb33fabceb65a528c2706e2eede444f483c8f2e1
jones <jmmarao@gmail.com>
2024-05-08 22:30:11 -0300

    Main view design

--- src/main/java/com/jmmarao/workshopjavafxjdbc/MainApplication.java
+++ src/main/java/com/jmmarao/workshopjavafxjdbc/MainApplication.java
@@ -3,18 +3,24 @@ package com.jmmarao.workshopjavafxjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainView.fxml"));
+
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene mainScene = new Scene(scrollPane);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Sample JavaFX Application");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
